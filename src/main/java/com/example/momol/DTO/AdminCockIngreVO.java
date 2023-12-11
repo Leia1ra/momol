9d1e05fd3a18735bbb6334a547848e027cce7c66
play4uu <94965592+play4yuu@users.noreply.github.com>
2023-12-11 17:28:49 +0900

    AdminCockIngreVO 추가

---
+++ src/main/java/com/example/momol/DTO/AdminCockIngreVO.java
@@ -0,0 +1,13 @@
+package com.example.momol.DTO;
+
+import lombok.Data;
+
+@Data
+public class AdminCockIngreVO {
+
+    String name;
+    int ing_num;
+    String ing_amount;
+    String ing_name;
+
+}
