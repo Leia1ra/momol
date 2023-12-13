4c1086d8be40f2a1a3bdc9b0fee9d88f7759f921
eogks <sinto6667@naver.com>
2023-12-13 11:51:34 +0900

    넣기

--- src/main/java/com/example/momol/Controller/CommunityController.java
+++ src/main/java/com/example/momol/Controller/CommunityController.java
@@ -6,13 +6,23 @@ import com.example.momol.DTO.CommunityVO;
 import com.example.momol.Service.CommentService;
 import com.example.momol.Service.CommunityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
+import java.util.UUID;
 
 @Controller
 @RequestMapping("/community")
@@ -133,9 +143,49 @@ public class CommunityController {
     }
 
 
+    private String saveUploadedFile(MultipartFile file) throws IOException {
+        // 업로드할 디렉토리 경로를 설정 (프로젝트 내의 원하는 위치로 설정 가능)
+        String uploadDir = "path/to/upload/directory";
+
+        // 업로드할 디렉토리가 없으면 생성
+        File uploadDirFile = new File(uploadDir);
+        if (!uploadDirFile.exists()) {
+            uploadDirFile.mkdirs();
+        }
+
+        // 파일명 중복을 피하기 위해 UUID를 사용하여 파일명 생성
+        String fileName = UUID.randomUUID().toString() + "_" + StringUtils.cleanPath(file.getOriginalFilename());
+
+        // 파일을 업로드할 경로 설정
+        Path uploadPath = Path.of(uploadDir, fileName);
+
+        // 파일 복사
+        Files.copy(file.getInputStream(), uploadPath, StandardCopyOption.REPLACE_EXISTING);
+
+        return fileName;
+    }
+
     @PostMapping("/posting")
-    public String writePost(CommunityVO vo, RedirectAttributes redirectAttributes) {
+    public String writePost(
+            CommunityVO vo,
+            @RequestPart(value = "file", required = false) MultipartFile file,
+            RedirectAttributes redirectAttributes) {
         System.out.println(">" + vo.toString());
+
+        // 파일 업로드 처리
+        if (file != null && !file.isEmpty()) {
+            try {
+                // 파일 업로드 로직 적용
+                String fileName = saveUploadedFile(file);
+
+                // 이제 setFileName 메서드를 사용하여 fileName을 vo에 설정
+                vo.setFileName(fileName);
+            } catch (IOException e) {
+                e.printStackTrace();
+                // 파일 업로드 중 오류 발생 시, 적절한 처리를 수행하거나 예외를 던질 수 있습니다.
+            }
+        }
+
         int result = service.communityInsert(vo);
 
         // 리다이렉트 시에 데이터 전달
@@ -145,5 +195,30 @@ public class CommunityController {
         return "redirect:/"; // 리다이렉트할 경로를 적절하게 수정
     }
 
+    @GetMapping("/delete/{num}")
+    public String deletePost(@PathVariable int num, RedirectAttributes redirectAttributes) {
+        int result = service.deletePost(num);
+
+        if (result > 0) {
+            redirectAttributes.addFlashAttribute("message", "게시글이 삭제되었습니다.");
+        } else {
+            redirectAttributes.addFlashAttribute("message", "게시글 삭제에 실패했습니다.");
+        }
+
+        return "redirect:/community/walls";
+    }
+
+    @PostMapping("/walls/{num}")
+    @ResponseBody
+    public ResponseEntity<String> likePost(@PathVariable int num) {
+        // 좋아요 처리 로직 추가
+        int updatedLikes = service.updateLikes(num);
+
+        if (updatedLikes > 0) {
+            return new ResponseEntity<>("Liked post " + num, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Failed to update likes for post " + num, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
 }
--- src/main/java/com/example/momol/Service/CommunityService.java
+++ src/main/java/com/example/momol/Service/CommunityService.java
@@ -1,11 +1,18 @@
 package com.example.momol.Service;
 
 import com.example.momol.DTO.CommunityVO;
-import org.springframework.stereotype.Service;
 
 
 public interface CommunityService {
     int communityInsert(CommunityVO vo);
+
     CommunityVO getPostByNum(int num);
 
+    int deletePost(int num);
+
+    int updateLikes(int num);
+
+    int getLikes(int num);
+
+    int updateLikes(int num, int likes);
 }
