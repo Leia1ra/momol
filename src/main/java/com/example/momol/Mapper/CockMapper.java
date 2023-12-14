5b966e23b15f66eb8d59224c8bec48e3fb7f1d03
kimji <jin00802@naver.com>
2023-12-14 18:37:07 +0900

    칵테일 필터추가(베이스주,맛만)

--- src/main/java/com/example/momol/Mapper/CockMapper.java
+++ src/main/java/com/example/momol/Mapper/CockMapper.java
@@ -13,7 +13,7 @@ public interface CockMapper {
     List<CockIngredientVO> cock_ingreList(String name);
     List<CocktailVO> searchCocktails(String searchText);
     List<CockIngredientVO> cock_ingre(String name);
-
+    List<CocktailVO> getCategoryData2(CocktailVO vo);
 
 
 }
--- src/main/resources/static/Cocktail/pagechange.js
+++ src/main/resources/static/Cocktail/pagechange.js
@@ -26,6 +26,7 @@ function changeActive(element) {
 }
 
 
+
 function searchCocktails(searchText,context) {
     console.log(searchText);
     $.ajax({
@@ -40,14 +41,14 @@ function searchCocktails(searchText,context) {
                 inner += `
                  <div class="grid-item">
                     <a href="${context}/Cocktail/cakinfo?name=${r.name}">                  
-                        <img src=${r.cocktail_img} alt="게시물1썸네일" class="thumbnail">
+                        <img src="${r.cocktail_img}" alt="게시물1썸네일" class="thumbnail">
                         <div>${r.name}</div>
                         <div class="dz">${r.cocktail_detail}</div>
                     </a>
                     <div class="tags">
-                        <div class="tag1"></div>
-                        <div class="tag2"></div>
-                        <div class="tag3"></div>
+                        <div class="tag1">${r.basetag}</div>
+                        <div class="tag2">${r.tastetag}</div>
+                        <div class="tag3">${r.smelltag}</div>
                     </div>
                 </div>
                 `
@@ -59,6 +60,71 @@ function searchCocktails(searchText,context) {
         }
     });
 }
+
+document.addEventListener("DOMContentLoaded", function (event) {
+    const formData = new FormData();
+    document.getElementById('select_base').addEventListener('change', function() {
+            formData.set("basetag",this.value)
+
+        filterData(formData);
+    });
+    document.getElementById('select_base2').addEventListener('change', function() {
+        formData.set("ABV",this.value)
+        filterData(formData);
+    });
+    document.getElementById('select_base3').addEventListener('change', function() {
+        formData.set("tastetag",this.value)
+        filterData(formData);
+    });
+})
+
+function filterData(formData) {
+    fetch('/Cocktail/getCategoryData2',{
+        method:"POST",
+        headers:{
+
+        }, body:formData
+    }).then((result) =>{
+        return result.json()
+    }).then((data)=>{
+        console.log("Response data:", data);
+        let container = document.getElementsByClassName('grid-container')[0];
+        let inner = "";
+        data.forEach(function (r) {
+                inner += `
+             <div class="grid-item">
+                <a href="$/Cocktail/cakinfo?name=${r.name}">                  
+                    <img src="${r.cocktail_img}" alt="게시물1썸네일" class="thumbnail">
+                    <div>${r.name}</div>
+                    <div class="dz">${r.cocktail_detail}</div>
+                </a>
+                <div class="tags">
+                    <div class="tag1">${r.basetag}</div>
+                    <div class="tag2">${r.tastetag}</div>
+                    <div class="tag3">${r.smelltag}</div>
+                </div>
+            </div>
+            `
+        })
+        container.innerHTML= inner;
+    })
+    // $.ajax({
+    //     type: "GET",
+    //     url: "/Cocktail/getCategoryData2",
+    //     data: formData,
+    //     dataType: "json",
+    //     success: function (data) {
+    //
+    //     },
+    //     error: function (error) {
+    //         console.error("Error during category filter:", error);
+    //     }
+    // });
+}
+
+
+
+
 
 
 
