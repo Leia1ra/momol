3dd973f522bc062bd3ea4987458b14b7ed7e43a0
kimji <jin00802@naver.com>
2023-12-17 23:00:12 +0900

    관련칵테일 href 수정

--- src/main/java/com/example/momol/Mapper/IngredientMapper.java
+++ src/main/java/com/example/momol/Mapper/IngredientMapper.java
@@ -11,7 +11,6 @@ public interface IngredientMapper {
     List<IngredientVO> ingredientList();
     IngredientVO jaeryoinfo(int ing_num);
     List<IngredientVO> jaeryoList(int ing_num);
-
     List<IngredientVO> searchCocktails2(String searchText);
     List<IngredientVO> getCategoryData(String category);
     List<CocktailVO> make_list (String name);
